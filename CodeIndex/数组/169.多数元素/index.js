8e5417545167b12be732328384ad3ba92d727147
yuguang <yuguang@gaoding.com>
2019-12-31 14:19:41 +0800

    Updates

--- CodeIndex/数组/169.多数元素/index.js
+++ CodeIndex/数组/169.多数元素/index.js
@@ -5,22 +5,37 @@
 //     return nums[Math.floor(nums.length/2)]
 // };
 
+// var majorityElement = function(nums) {
+//     var obj = {}
+//     var max = nums[0]
+//     for(let i = 0; i < nums.length; i++){
+//         if(obj[nums[i]]){
+//             obj[nums[i]] += 1
+//             if(obj[max] <= obj[nums[i]]){
+//                 max = nums[i]
+//             }
+//         }else{
+//             obj[nums[i]] = 1
+//         }
+//     }
+//     return max
+// };
+
 var majorityElement = function(nums) {
-    var obj = {}
-    var max = nums[0]
+    var idx = null
+    var count = 0
     for(let i = 0; i < nums.length; i++){
-        if(obj[nums[i]]){
-            obj[nums[i]] += 1
-            if(obj[max] <= obj[nums[i]]){
-                max = nums[i]
-            }
+        if(count ===0 ){
+            idx = nums[i]
+        }
+        if(idx === nums[i]){
+            count++
         }else{
-            obj[nums[i]] = 1
+            count--
         }
     }
-    return max
+    return idx
 };
-
 
 var res = majorityElement([2,2,1,1,1,2,2])
 console.log('res:', res);
