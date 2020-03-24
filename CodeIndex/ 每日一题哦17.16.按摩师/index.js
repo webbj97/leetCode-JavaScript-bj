a6f77cfcfa8c830609aa98d6844eefcba705d1a5
yuguang <yuguang@gaoding.com>
2020-03-24 14:46:22 +0800

    Updates

--- CodeIndex/ 每日一题哦17.16.按摩师/index.js
+++ CodeIndex/ 每日一题哦17.16.按摩师/index.js
@@ -25,11 +25,35 @@
  * @param {number[]} nums
  * @return {number}
  */
-var massage = function(nums) {
-    var max = 0
+// var massage = function(nums) {
+//     var arr = []
+//     var len = nums.length
+//     console.log('len:', len);
 
+//     if(!len) return 0;
+//     if(len === 1) return nums[0];
+//     arr[0] = nums[0]
+//     arr[1] = nums[0] > nums[1] ? nums[0] : nums[1]
+//     for(let i = 2; i < len; i++){
+//         arr[i] = Math.max(arr[i-1], arr[i-2]+nums[i])
+//     }
+//     return arr[len-1]
+// };
+
+var massage = function(nums) {
+    var a = 0, b = 0
+
+    for(let i = 0; i < nums.length; i++){
+        let c = Math.max(b, nums[i]+a) //兼容了数组长度为1和0的情况
+        a = b
+        b = c
+    }
+    return b
 };
 
+// 核心思想：dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i])
+
 var arr = [1,2,3,1]
+// var arr = []
 var res = massage(arr)
 console.log('res:', res);
