3442d20ba4e3d8bddb1c948f1f4079146db9895d
yuguang <yuguang@gaoding.com>
2020-04-28 09:30:40 +0800

    Updates

---
+++ CodeIndex/633.平方数之和/index.js
@@ -0,0 +1,16 @@
+/*
+ * @desc:
+ * @Author: 余光
+ * @Email: webbj97@163.com
+ * @Date: 2020-04-27 17:36:55
+ */
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function(c) {
+    var a = 1
+};
+
+var res = judgeSquareSum(10)
+console.log('res:', res);
